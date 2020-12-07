7f0d96989245bbd5becc3fd9a7e11264a9540017
manjunathabs <manjunathmanju761@gmail.com>
2020-12-07 20:45:37 +0530

    calculated logic for items
    added price calculated logic

--- price_cal.rb
+++ price_cal.rb
@@ -31,7 +31,43 @@ class SaleItem
     return @@sale_items
   end
 end
+class PriceCalculator
 
+  def generate_bill
+    # user input of customer items
+    puts "Please enter all the items purchased separated by a comma:"
+    @customer_items = gets.chomp.split(/,/).map(&:to_s)
+
+    items_quantity  = Hash.new(0)
+    @customer_items.each { |item| items_quantity[item] += 1 }
+    bill_summary    = calculate_total_bill(items_quantity)
+    bill_display(items_quantity, bill_summary)
+  end
+
+
+
+  private
+
+  def calculate_total_bill(items_quantity)
+    summary = Hash.new()
+    items_quantity.each do |product, value|
+
+      selling_price = Item.all[product]
+      sale_details  = SaleItem.all[product]
+
+      #  calculate the normal price if no sales items
+      if !sale_details.present?
+        cost_price  = items_quantity[product] * selling_price
+
+      else # Calculate the sale price and any extra products than the sale units
+        discount_price  = (items_quantity[product] / sale_details['units']) * sale_details['price']
+        cost_price      =  discount_price +  ((items_quantity[product] % sale_details['units']) * selling_price)
+      end
+
+      summary[product]  = cost_price
+    end
+    return summary
+  end
 
 
 
